3d336f5dc3d72719b5c58c1a1e5ede9cf6af2c9b
Cameron Wortlehock <cameron.wortlehock@readingroom.com>
2014-10-31 14:22:43 +0000

    Update ruby-sass Added package.json Added .gitignore Added .editorconfig

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,11 @@
-// Dependencies and plugins:
-var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass');
+var gulp, sass;
 
-// Set paths: 
-var paths = {
-  sass: 'src/scss/'
-};
+gulp = require('gulp');
+sass = require('gulp-ruby-sass');
 
-// Gulp tasks
-gulp.task('sass-to-css', function(){
-	return gulp.src(sassRoot+'main.scss')
-		.pipe(sass({sourcemap: true}))
-		.pipe(gulp.dest('dist/css'));
+gulp.task('default', function () {
+    return gulp.src('src/scss/*.scss')
+        .pipe(sass({sourcemap: true, sourcemapPath: '../scss'}))
+        .on('error', function (err) { console.log(err.message); })
+        .pipe(gulp.dest('dist/css'));
 });
